f136308ced8fadc37870d2ac15ca108b80a61ee3
MarianaSafiulin <143293934+MarianaSafiulin@users.noreply.github.com>
2024-04-07 21:49:48 +0300

    task3

--- js/tasks-1.js
+++ js/tasks-1.js
@@ -12,3 +12,16 @@ function getShippingMessage (country, price, deliveryFee){
 }
 
 console.log(getShippingMessage("Australia", 120, 50));
+
+function getElementWidth (content, padding, border){
+    const contentWidth = parseInt(content);
+    const paddingWidth = parseInt(padding);
+    const borderWidth = parseInt(border);
+
+ 
+let totalWidth = contentWidth + paddingWidth * 2  +  borderWidth * 2;
+
+return totalWidth;
+
+}
+console.log(getElementWidth("50px", "8px", "4px"));
